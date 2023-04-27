49822bcaef00465d74f1c11fc987f1a343e93030
ozgeterzioglu <ozgeterzioglu@yahoo.com>
2023-04-26 19:50:29 -0700

    cleaning up files & getting ready to clean stanford data

--- scripts/extract.py
+++ scripts/extract.py
@@ -8,16 +8,24 @@ for (root, dirs, files) in os.walk(".", topdown = True):
         file_name, file_extension = os.path.splitext(iterate)
         if file_extension == ".pdf":
             with pdfplumber.open(file_path) as pdf:
-                page = pdf.pages[0]
-                width = page.width
-                height = page.height
-                page = page.crop((0, 120, width, height - 30), relative = True)
-                table_settings = {"vertical_strategy": "lines",
+                for index, page in enumerate(pdf.pages): 
+                    width = page.width
+                    height = page.height
+                    page = page.crop((0, 120, width, height - 30), relative = True)
+                    table_settings = {"vertical_strategy": "lines",
                                        "horizontal_strategy": "text",
                                        "snap_y_tolerance": 2,
                                        "intersection_x_tolerance": 3, 
                                        "explicit_vertical_lines": [20, width - 20]}
-                extracted_data = page.extract_table(table_settings)
-                with open("berkeley__crime_data.csv", "a") as file:
-                    writer = csv.writer(file)
-                    writer.writerows(extracted_data)
+                    extracted_data = page.extract_table(table_settings)
+                    if extracted_data == None:
+                        print(file_path, index + 1)
+                        #TO DO TUESDAY: 
+                        #1: if it errors on page one, have it ignore the page or skip it
+                        #2: if it errors on page 2 or more, then have it implement a table 
+                        # // extraction method specified for pages without lines
+                        # side note: create this extraction method in jupyter notebook with visual debugger
+                    else:
+                        with open("berkeley__crime_data.csv", "a") as file:
+                            writer = csv.writer(file)
+                            writer.writerows(extracted_data)
