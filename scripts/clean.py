c94a14abaf5dfa1b886075ad46d220748d53942f
ozgeterzioglu <ozgeterzioglu@yahoo.com>
2023-04-28 23:41:52 -0700

    tried to work on it more, not successful

--- scripts/clean.py
+++ scripts/clean.py
@@ -15,6 +15,10 @@ df.columns = [col.lower().replace(" ", "_") for col in df.columns]
 df.insert(3, "date", None, allow_duplicates = True)
 df.insert(4, "time", None, allow_duplicates = True)
 
+for empty_row in df["nature"]:
+    if df["case_#"] and df["reported"] and df["occured"] and df["location"] and df["disposition"] and df["on_campus?"] and df["area"] == None:
+        df = 
+
 from datetime import datetime
 for entry in df["reported"]:
     if type(entry) != type(0.0):
@@ -32,6 +36,6 @@ for entry in df["reported"]:
 #whitespace trim for all data
 #append not in the right scope, needs to be after every entry 
 #need to make sure rows are not skipped when entering data into columns
-#add empty rows with crime into the one with full rows above it
+    #solution: add empty rows with crime into the one with full rows above it
 
 print(df)
