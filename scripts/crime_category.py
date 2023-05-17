13fb9b1d9056d6a48e9f42b7c4e0fc6f54289c68
ozgeterzioglu <ozgeterzioglu@yahoo.com>
2023-05-16 21:21:46 -0700

    finished data analysis to viz pipeline

--- scripts/crime_category.py
+++ scripts/crime_category.py
@@ -2,63 +2,67 @@ import csv
 import pandas as pd
 import os
 from datetime import datetime 
-
 from pathlib import Path
-base_dir = Path(__file__).parents[1]
-path = os.path.join(base_dir, "data/processed/stanford_crime_clean.csv")
-df = pd.read_csv(path)
 
-df.insert(5, "year", None, allow_duplicates = True)
+def standardize_crimes():
+    base_dir = Path(__file__).parents[1]
+    path = os.path.join(base_dir, "data/processed/stanford_crime_clean.csv")
+    df = pd.read_csv(path)
 
-for iterate, date in df["date"].iteritems():
-    date_object = datetime.strptime(date, "%m/%d/%Y")
-    clean_year = date_object.strftime("%Y")
-    df.loc[iterate, "year"] = clean_year
+    df.insert(5, "year", None, allow_duplicates = True)
 
-df["nature"] = df["nature"].str.replace(r'[/\:]',' ').str.lower()
+    for iterate, date in df["date"].iteritems():
+        date_object = datetime.strptime(date, "%m/%d/%Y")
+        clean_year = date_object.strftime("%Y")
+        df.loc[iterate, "year"] = clean_year
 
-crime_flags = {"Homicide": ["homicide", "manslaughter"],
-               "Sexual assault": ["lewd", "child", "peek in hole", "indecent exposure", 
-                                  "peek", "sodomy", "oral", "sexual", "rape", "underwear", "sex"], 
-                "Burglary": ["burglary", "burg", "robbery"], 
-                "Assault": ["assault", "battery", "aggrevated", "injury", "false imprisonment"],
-                "Theft": ["petty theft", "lost property", "grand theft", "grnd thft",
-                          "carjacking", "theft", "shoplifting", "stolen"],
-                "Arson": ["fire"], 
-                "Destruction of property": ["vandl", "vandalism", "deface", "damage"],
-                "Domestic violence": ["battery spouse", "spouse", "dom violence"],
-                "Stalking": ["stalking"],
-                "Weapons law violations": ["deadly weapon", "firearm", "weapon", "pos wpn", "exhibit"],
-                "Drug abuse violations": ["paraphernalia", "dui", "alcohol", "drug", "controlled substance",
-                                          "cntl sub", "marijuana", "heroin", "smoking", "nitrous oxide"],
-                "Liquor law violations": ["liquor", "alcohol", "open container"], 
-                "Fraud": ["defraud", "false pretenses", "identity theft", "impersonation", "extortion", "fraud", "embez"],
-                "Annoying phone calls": ["annoy"],
-                "Hit and Run": ["hit"],
-                "Hate crime": ["swastika", "vio civil rghts", "noose", "hate"], 
-                "Non-Clery Act": ["animal bite", "abandoned vehicle", "cruelty to animals", "fire", "dis cndct", 
-                                 "disord conduct", "disorderly conduct", "leash", "loiter", "loud", "drive",
-                                 "tresspass", "trspss", "eavesdropping", "warrant", "wilfl dschrg", "offensive words", 
-                                 "no id", "display unauth placard", "contempt", "false re", "nuisance", "unlawful",
-                                 "disrupt", "disturb", "disturbance", "enter", "evading", "false bomb", "false id",
-                                 "terrorize", "fail to", "obstruct", "reckless"]}
+    df["nature"] = df["nature"].str.replace(r'[/\:]',' ').str.lower()
 
-crime_count = {}
+    crime_flags = {"Homicide": ["homicide", "manslaughter"],
+                "Sexual assault": ["lewd", "child", "peek in hole", "indecent exposure", 
+                                    "peek", "sodomy", "oral", "sexual", "rape", "underwear", "sex"], 
+                    "Burglary": ["burglary", "burg", "robbery"], 
+                    "Assault": ["assault", "battery", "aggrevated", "injury", "false imprisonment"],
+                    "Theft": ["petty theft", "lost property", "grand theft", "grnd thft",
+                            "carjacking", "theft", "shoplifting", "stolen"],
+                    "Arson": ["fire"], 
+                    "Destruction of property": ["vandl", "vandalism", "deface", "damage"],
+                    "Domestic violence": ["battery spouse", "spouse", "dom violence"],
+                    "Stalking": ["stalking"],
+                    "Weapons law violations": ["deadly weapon", "firearm", "weapon", "pos wpn", "exhibit"],
+                    "Drug abuse violations": ["paraphernalia", "dui", "alcohol", "drug", "controlled substance",
+                                            "cntl sub", "marijuana", "heroin", "smoking", "nitrous oxide"],
+                    "Liquor law violations": ["liquor", "alcohol", "open container"], 
+                    "Fraud": ["defraud", "false pretenses", "identity theft", "impersonation", "extortion", "fraud", "embez"],
+                    "Annoying phone calls": ["annoy"],
+                    "Hit and Run": ["hit"],
+                    "Hate crime": ["swastika", "vio civil rghts", "noose", "hate"], 
+                    "Non-Clery Act": ["animal bite", "abandoned vehicle", "cruelty to animals", "fire", "dis cndct", 
+                                    "disord conduct", "disorderly conduct", "leash", "loiter", "loud", "drive",
+                                    "tresspass", "trspss", "eavesdropping", "warrant", "wilfl dschrg", "offensive words", 
+                                    "no id", "display unauth placard", "contempt", "false re", "nuisance", "unlawful",
+                                    "disrupt", "disturb", "disturbance", "enter", "evading", "false bomb", "false id",
+                                    "terrorize", "fail to", "obstruct", "reckless"]}
 
-for index, row in df.iterrows():
-    year = row["year"]
-    nature = row["nature"]
+    crime_count = {}
 
-    if year not in crime_count:
-        crime_count[year] = {key: 0 for key in crime_flags.keys()}
-    
-    words = nature.split()
-    for key, value in crime_flags.items():
-        if any(word.lower() in value for word in words):
-            crime_count[year][key] += 1
+    for index, row in df.iterrows():
+        year = row["year"]
+        nature = row["nature"]
 
-count_df = pd.DataFrame.from_dict(crime_count, orient = "index")
-count_df = count_df.rename_axis("Year")
-count_df.columns = crime_flags.keys()
+        if year not in crime_count:
+            crime_count[year] = {key: 0 for key in crime_flags.keys()}
+        
+        words = nature.split()
+        for key, value in crime_flags.items():
+            if any(word.lower() in value for word in words):
+                crime_count[year][key] += 1
 
-count_df.to_csv("data/processed/crime_categories.csv")
+    count_df = pd.DataFrame.from_dict(crime_count, orient = "index")
+    count_df = count_df.rename_axis("Year")
+    count_df.columns = crime_flags.keys()
+
+    count_df.to_csv("data/processed/crime_categories.csv")
+
+if __name__ == "__main__":
+    standardize_crimes()
---
+++ scripts/run_viz_pipeline.py
@@ -0,0 +1,9 @@
+from crime_category import standardize_crimes
+from data_to_viz import vizualize
+
+def main():
+    standardize_crimes
+    vizualize
+
+if __name__ == "__main__":
+    main()
