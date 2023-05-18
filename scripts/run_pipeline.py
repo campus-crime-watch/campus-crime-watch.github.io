30fb98a6d4b76cb9f52d634a1d675e1d865a9e2a
ozgeterzioglu <ozgeterzioglu@yahoo.com>
2023-05-17 19:12:22 -0700

    added crime category to pipeline

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -1,6 +1,8 @@
 from extract import grab_data
 from pre_process import merge, clean_date
 from clean_geocode import geocode_pipeline
+from crime_category import standardize_crimes
+from data_to_viz import vizualize 
 from csv_to_geojson import to_json
 
 """
@@ -26,6 +28,14 @@ def main():
     # input: stanford_crime_clean.csv in data/processed
     # output: stanford_crime_clean_geocoded.csv in data/processed
     geocode_pipeline()
+
+    # input: stanford_crime_clean.csv in data/processed
+    # output: crime_categories.csv in data/processed
+    standardize_crimes()
+
+    # input: crime_categories.csv in data/processed
+    # output: NONE RIGHT NOW,,,, should be HTML code? add it to JSON file? i don't know
+    vizualize()
 
     # input: stanford_crime_clean_geocoded.csv in data/processed
     # output: stanford_crime.geojson in docs/data
