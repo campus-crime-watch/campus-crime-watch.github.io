df6dad6844fd1ccb891c0e1056ef7c9fb2662d70
ozgeterzioglu <ozgeterzioglu@yahoo.com>
2023-05-31 17:06:11 -0700

    reworked sentences

--- docs/sentences.js
+++ docs/sentences.js
@@ -14,48 +14,55 @@ const config = {
     }
 };
 
-fetch('data/stat_sentences.json')
-  .then(response => response.json())
-  .then(data => {
-    const jsonContent = document.getElementById('json-content');
+document.addEventListener('DOMContentLoaded', function () {
 
-    data.forEach(sentence => {
-      const parts = sentence.split(' ');
-      const count = parts[2];
-      const category = parts.slice(3, parts.length - 2).join(' ');
+  const jsonContent = document.getElementById('json-content');
 
-      const sentenceElement = document.createElement('p');
-      sentenceElement.classList.add('json-sentence');
-      const countElement = document.createElement('span');
-      const categoryElement = document.createElement('span');
+  fetch('data/stat_sentences.json')
+    .then(response => response.json())
+    .then(data => {
 
-      countElement.textContent = count;
-      categoryElement.textContent = category;
+      data.forEach(sentence => {
+        const parts = sentence.split(' ');
+        const count = parts[2];
+        const category = parts.slice(3, parts.length - 2).join(' ');
 
-      countElement.classList.add('emphasis');
-      categoryElement.classList.add('emphasis');
+        const sentenceElement = document.createElement('p');
+        sentenceElement.classList.add('json-sentence');
+        const countElement = document.createElement('span');
+        const categoryElement = document.createElement('span');
 
-      sentenceElement.appendChild(document.createTextNode('There were '));
-      sentenceElement.appendChild(countElement);
-      sentenceElement.appendChild(document.createTextNode(' reported '));
-      sentenceElement.appendChild(categoryElement);
-      sentenceElement.appendChild(document.createTextNode(' in ' + parts[parts.length - 1] + '.'));
+        countElement.textContent = count;
+        categoryElement.textContent = category;
 
-      jsonContent.appendChild(sentenceElement);
-      jsonContent.appendChild(document.createElement('br'));
-    });
-  })
-  .catch(error => {
-    console.log('Error:', error);
+        countElement.classList.add('emphasis');
+        categoryElement.classList.add('emphasis');
+        sentenceElement.classList.add('rest-of-sentence');
+
+        sentenceElement.appendChild(document.createTextNode('There were '));
+        sentenceElement.appendChild(countElement);
+        sentenceElement.appendChild(document.createTextNode(' reported '));
+        sentenceElement.appendChild(categoryElement);
+        sentenceElement.appendChild(document.createTextNode(' in ' + parts[parts.length - 1]));
+
+        jsonContent.appendChild(sentenceElement);
+        jsonContent.appendChild(document.createElement('br'));
+        jsonContent.appendChild(document.createElement('br'));
+        jsonContent.appendChild(document.createElement('br'));
+      });
+    })
+
+    .catch(error => {
+      console.log('Error:', error);
+
   });
-   
+});
+
 const scroll = ScrollReveal(config);
 const left = document.querySelectorAll(".text-holder.left");
-const sentence = document.querySelectorAll(".json-sentence");
-const filter = document.querySelectorAll(".input-wrapper");
 const graph = document.getElementById("my_dataviz");
+const filter = document.querySelectorAll(".input-wrapper");
 
 scroll.reveal(left, {delay: 1000});
-scroll.reveal(sentence, {delay: 1000});
 scroll.reveal(graph, {delay: 1500});
-scroll.reveal(filter, {delay: 2000});
+scroll.reveal(filter, {delay: 1750});
