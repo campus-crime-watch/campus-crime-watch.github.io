3b07af22d3ab3ac9c507aac495905d2b90154433
ozgeterzioglu <ozgeterzioglu@yahoo.com>
2023-06-01 11:53:53 -0700

    fixed sentence fomatting

--- docs/sentences.js
+++ docs/sentences.js
@@ -17,53 +17,47 @@ const config = {
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-
   const jsonContent = document.getElementById('json-content');
 
-  //grab sentences that we created in crime_category.py from json file
   fetch('data/stat_sentences.json')
     .then(response => response.json())
     .then(data => {
-
-      //iterate through each sentence and split it into its respective parts
       data.forEach(sentence => {
         const parts = sentence.split(' ');
         const count = parts[2];
-        const categoryParts = parts.slice(3, parts.length - 2);
+        const categoryParts = parts.slice(3, parts.length - 3);
         const category = categoryParts.join(' ');
 
         const sentenceElement = document.createElement('p');
         sentenceElement.classList.add('json-sentence');
+
         const countElement = document.createElement('span');
-        const categoryElement = document.createElement('span');
-
-        //get the count (number) from each sentence and the crime category
         countElement.textContent = count;
-        categoryElement.textContent = ' ' + category;
-
-        // create CSS classes so we can style the number, crime category, and rest of sentence seperately 
         countElement.classList.add('emphasis');
-        categoryElement.classList.add('emphasis');
-        sentenceElement.classList.add('rest-of-sentence');
 
-        //reconstruct the sentence back to one piece to be displayed as a sentence instead of individual parts 
+        const categoryElement = document.createElement('span');
+        categoryElement.textContent = category;
+        categoryElement.classList.add('emphasis');
+
+        const restOfSentenceElement = document.createElement('span');
+        restOfSentenceElement.classList.add('rest-of-sentence');
+        restOfSentenceElement.textContent = ' ' + parts[parts.length - 3] + ' ' + parts[parts.length - 2] + ' ' + parts[parts.length - 1];
+
         sentenceElement.appendChild(document.createTextNode('There were '));
         sentenceElement.appendChild(countElement);
+        sentenceElement.appendChild(document.createTextNode(' '));
         sentenceElement.appendChild(categoryElement);
-        sentenceElement.appendChild(document.createTextNode(' in ' + parts[parts.length - 1]));
+        sentenceElement.appendChild(document.createTextNode(restOfSentenceElement.textContent));
 
-        //add the complete sentence to our log & insert breaks between each sentence so it's not displayed in a giant chunk
         jsonContent.appendChild(sentenceElement);
         jsonContent.appendChild(document.createElement('br'));
         jsonContent.appendChild(document.createElement('br'));
         jsonContent.appendChild(document.createElement('br'));
       });
     })
-
     .catch(error => {
       console.log('Error:', error);
-
-  });
+    });
 });
 
 // create variables for the ScrollReveal effects
