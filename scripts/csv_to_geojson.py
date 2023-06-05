653cfe768584fb579c25d7b2e0c1265ce9593a44
Isabel-S <isabelsieh@gmail.com>
2023-06-05 13:49:26 -0700

    cleaned up geocoding code

--- scripts/csv_to_geojson.py
+++ scripts/csv_to_geojson.py
@@ -6,6 +6,7 @@ def to_geojson():
     csv_file = 'data/processed/stanford_crime_clean_geocoded_categorized.csv'  
     data = pd.read_csv(csv_file)
 
+    # renames x_long, y_lat
     data = data.rename(columns={'x_long':'longitude','y_lat':'latitude'})
 
     geometry = [Point(xy) for xy in zip(data.longitude, data.latitude)]
